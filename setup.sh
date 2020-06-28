aca085086efc4c203fee24718a8664636474b0dc
zhangddjs <1005155946@qq.com>
2020-06-28 11:37:31 +0800

    20200628

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
+#!bin/bash
+
 slave_user="slave"
 slave_password="123456"
 root_password="123456"
 master_container="mysql-master"
 docker network create mysql-cluster
 docker-compose -f master/master.yml up -d
-until docker exec -it mysql-master mysql -uroot -p'$root_password' -e ";"
+until docker exec -it mysql-master mysql -uroot -p$root_password -e ";"
   do
       echo "mysql-master loading....(will retry several times)"
       sleep 5
   done
 create_user='GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO "'$mysql_user'"@"%" IDENTIFIED BY "'$mysql_password'"; FLUSH PRIVILEGES;'
-docker exec -it $master_container sh -c 'mysql -uroot -p '$root_password' -e '$create_user''
-docker exec -it $master_container sh -c 'mysql -uroot -p '$root_password' -e "show master status;"'
+docker exec -it $master_container mysql -uroot -p$root_password -e $create_user
+docker exec -it $master_container mysql -uroot -p$root_password -e show master status;
+
+exit 0
