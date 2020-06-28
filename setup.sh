431bcf334be1b5d88b153adb027b80dec5ee0388
zhangddjs <1005155946@qq.com>
2020-06-28 11:29:13 +0800

    20200628

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ root_password="123456"
 master_container="mysql-master"
 docker network create mysql-cluster
 docker-compose -f master/master.yml up -d
-until docker exec mysql-master sh -c 'mysql -uroot -p '$root_password' -e ";"'
+until docker exec -it mysql-master mysql -uroot -p'$root_password' -e ";"
   do
       echo "mysql-master loading....(will retry several times)"
       sleep 5
