dfcbf3e9bdf7ac20b4fe7d03fef4ced1e04d4964
Dongdong Zhang <dongdzha@cisco.com>
2020-06-28 21:26:09 +0800

    20200628

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-slave_user="slave"
-slave_password="123456"
+cluster_user="slave"
+cluster_password="123456"
 root_password="123456"
 master_container="mysql-master"
-docker network create mysql-cluster
-docker-compose -f master/master.yml up -d
-until docker exec -it $master_container mysql -uroot -p$root_password -e ";"
+slave_container="mysql-slave"
+docker-compose down
+rm -rf ./master/data/* ./slave/data/*
+docker-compose up -d
+until docker exec $master_container sh -c 'export MYSQL_PWD='$root_password';mysql -uroot -e ";"'
   do
       echo "mysql-master loading....(will retry several times)"
-      sleep 5
+      sleep 4
   done
-create_user='GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO "'$slave_user'"@"%" IDENTIFIED BY "'$slave_password'"; FLUSH PRIVILEGES;'
-docker exec -it $master_container mysql -uroot -p$root_password -e "'$create_user'"
-docker exec -it $master_container mysql -uroot -p$root_password -e "show master status;"
+create_user_stmt="GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO '$cluster_user'@'%' IDENTIFIED BY '$cluster_password'; FLUSH PRIVILEGES;"
+docker exec $master_container sh -c "export MYSQL_PWD='$root_password';mysql -u root -e '$create_user_stmt'"
+
+master_status=`docker exec $master_container sh -c 'export MYSQL_PWD='$root_password';mysql -u root -e "show master status;"'`
+log_name=`echo $master_status | awk '{print $6}'`
+log_pos=`echo $master_status | awk '{print $7}'`
+#master_ip=`docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $master_container`
+set_slave_stmt='change master to master_host='$master_container', master_user='$cluster_user', master_password='$cluster_password', master_port=3306, master_log_file='$log_name', master_log_pos=$log_pos;'
+
 
 exit 0
